44e76f6766cb11c39716730c3ffe59ba0589d87a
A W <AW@as-macbook-pro.local>
2019-08-27 22:17:47 -0400

    Initial Commit

---
+++ Project 25 - Selfie Share/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 25 - Selfie Share
+//
+//  Created by A W on 2019-08-27.
+//  Copyright © 2019 A W. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
